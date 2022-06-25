2c2bca7d5adaf373c4c8483e7c75a6299375ddd1
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-25 19:50:14 +0530

    Add HttpClient and add getMovies method

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=f3c98b285e47795a9a7b680a8823a475'
+    );
+  }
 }
