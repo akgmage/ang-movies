1190ae77b0f7f29f0a99c457ebaccb96ec32bd06
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-25 19:39:00 +0530

    Add wildcard route and Add routerlinks to Home and movies component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { MoviesComponent } from './pages/movies/movies.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'movies', component: MoviesComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
