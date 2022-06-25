33c0ef78a5f72db9ed36013c1f88ff9a067196f2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-25 20:06:29 +0530

    On init subscribe to getMovies method and assign response to movies array

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  movies: any = [];
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.moviesService.getMovies().subscribe((response: any) => {
+      this.movies = response.results;
+      console.log(this.movies);
+    });
   }
-
 }
