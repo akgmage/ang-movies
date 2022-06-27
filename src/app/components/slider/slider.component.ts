53184013d7a6b564e87653d4e3474729765aaaeb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-27 23:36:56 +0530

    Expect items from parent

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'slider',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
-
+  @Input() items:any;
+  
   constructor() { }
 
   ngOnInit(): void {
