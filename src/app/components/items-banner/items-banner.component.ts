442a8013c4bc4fbf209222b7494c3c48df7fadb5
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-28 22:48:02 +0530

    Show items-banner inside home component

--- src/app/components/items-banner/items-banner.component.ts
+++ src/app/components/items-banner/items-banner.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-items-banner',
+  selector: 'items-banner',
   templateUrl: './items-banner.component.html',
-  styleUrls: ['./items-banner.component.scss']
+  styleUrls: ['./items-banner.component.scss'],
 })
 export class ItemsBannerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
