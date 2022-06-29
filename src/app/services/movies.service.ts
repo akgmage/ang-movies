6c180974d3bf6b4617d77d97a4125d9b142b6b43
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-29 23:01:18 +0530

    Extract baseurl and API key

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -4,11 +4,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class MoviesService {
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  apiKey: string = 'f3c98b285e47795a9a7b680a8823a475';
   constructor(private http: HttpClient) {}
 
-  getMovies() {
+  getMovies(type: string = 'popular') {
     return this.http.get(
-      'https://api.themoviedb.org/3/movie/popular?api_key=f3c98b285e47795a9a7b680a8823a475'
+      `${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`
     );
   }
 }
