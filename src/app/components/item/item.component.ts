fb6aa2108f1533fcddc2c2a293b5b624421290e7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-30 22:11:12 +0530

    Import imagessizes constant

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/movie';
-
+import { IMAGES_SIZES } from 'src/app/constants/images-sizes';
 @Component({
   selector: 'item',
   templateUrl: './item.component.html',
@@ -8,6 +8,7 @@ import { Movie } from 'src/app/models/movie';
 })
 export class ItemComponent implements OnInit {
   @Input() itemData: Movie | null = null;
+  imagesSizes = IMAGES_SIZES;
   constructor() {}
 
   ngOnInit(): void {}
