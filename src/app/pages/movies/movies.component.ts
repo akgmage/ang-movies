ad780480db7a3caa0254dd57c695b7af550d27ec
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-01 23:03:39 +0530

    Inject MoviesService

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
+  movies: Movie[] = [];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
