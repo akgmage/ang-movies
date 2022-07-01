5e54096ee4e4c4850f3aa26430b4eb54b0e32a19
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-01 22:41:07 +0530

    Add component additive

---
+++ src/app/additive/additive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-additive',
+  templateUrl: './additive.component.html',
+  styleUrls: ['./additive.component.scss']
+})
+export class AdditiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SliderComponent } from './components/slider/slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
 import { ItemComponent } from './components/item/item.component';
+import { AdditiveComponent } from './additive/additive.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +23,7 @@ import { ItemComponent } from './components/item/item.component';
     SliderComponent,
     ItemsBannerComponent,
     ItemComponent,
+    AdditiveComponent,
   ],
   imports: [
     BrowserModule,
