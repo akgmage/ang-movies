a822f1bc3c5aeaa3cfbcbc42679ca625c0fe424c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-02 15:01:22 +0530

    Add getPagedMovied method and call first page number in oninit method

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -13,11 +13,14 @@ export class MoviesComponent implements OnInit {
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.moviesService.searchMovies(2).subscribe((movies) => {
+    this.getPagedMovies(1);
+  }
+  getPagedMovies(page: number) {
+    this.moviesService.searchMovies(page).subscribe((movies) => {
       this.movies = movies;
     });
   }
   paginate(event: any) {
-    console.log(event);
+    this.getPagedMovies(event.page + 1);
   }
 }
