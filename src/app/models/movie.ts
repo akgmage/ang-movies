da51965192b979e22757cc2e451fd578e9caca71
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-05 22:42:26 +0530

    Add revenue, runtime and status in model

--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -13,6 +13,9 @@ export interface Movie {
   video: boolean;
   vote_average: number;
   vote_count: number;
+  revenue: number;
+  runtime: number;
+  status: string;
 }
 
 export interface MovieDto {
