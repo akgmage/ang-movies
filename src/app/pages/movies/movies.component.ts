22d1f79f53c9610fed505efc6f5471fb5288e82d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-09 23:02:31 +0530

    Add get movies by genre

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Movie } from 'src/app/models/movie';
 import { MoviesService } from 'src/app/services/movies.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movies',
@@ -10,16 +12,26 @@ import { MoviesService } from 'src/app/services/movies.service';
 export class MoviesComponent implements OnInit {
   movies: Movie[] = [];
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(
+    private moviesService: MoviesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.getPagedMovies(1);
+    this.route.params.pipe(take(1)).subscribe(({ genreId }) => {
+      if (genreId) {
+        this.getMoviesByGenre(genreId);
+      } else {
+        this.getPagedMovies(1);
+      }
+    });
   }
   getPagedMovies(page: number) {
     this.moviesService.searchMovies(page).subscribe((movies) => {
       this.movies = movies;
     });
   }
+  getMoviesByGenre(genreId: string) {}
   paginate(event: any) {
     this.getPagedMovies(event.page + 1);
   }
